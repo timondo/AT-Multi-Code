08ca141dcc762b99df1716bdc7c0e09ce5793a8e
maltemelzer <maltemelzer@gmx.de>
2025-06-19 21:35:09 +0200

    add base_url and .env

--- Client.py
+++ Client.py
@@ -1,6 +1,15 @@
+import os
+from dotenv import load_dotenv
 from alpha_trader.client import Client
 
-client = Client(username="MY_USERNAME", password="MY_PASSWORD", partner_id="MY_PARTNER_ID")
+load_dotenv()
+
+client = Client(
+    base_url="https://stable.alpha-trader.com",
+    username=os.getenv("USERNAME"),
+    password=os.getenv("PASSWORD"),
+    partner_id=os.getenv("PARTNER_ID")
+)
 client.login()
 miner = client.get_miner()
 miner.transfer_coins()
